88284f3605300420789dade3740c0eb234026661
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-07-19 02:44:00 +0300

    feat: add router in the frentend during sign up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,12 @@
-import "./App.css";
-import axios from "axios";
-import { useState } from "react";
+import "./styles/App.css";
+import { Routes, Route } from "react-router-dom";
+import SignUp from "./pages/SignUp";
 
 function App() {
-  const [dbData, setDbData] = useState({});
-
-  async function fetchData() {
-    try {
-      const response = await axios.get("http://127.0.0.1:8000/patient");
-
-      setDbData(response.data);
-      console.log(response.data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  }
   return (
-    <>
-      <button onClick={fetchData}>Fetch Data</button>
-      <div>
-        {dbData.data?.map((data, index) => {
-          return (
-            <div key={index}>
-              <p>{data.id}</p>
-              <p>{data.created_at}</p>
-              <p>{data.age}</p>
-              <p>{data.gender}</p>
-            </div>
-          );
-        })}
-      </div>
-    </>
+    <Routes>
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 
