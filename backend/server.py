3c83d625c8d49a0058dd8b8e133be9a201f19f9e
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-07-18 20:09:54 +0300

    feat: update backend to post supabase db tables

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI 
 import os
+from dotenv import load_dotenv
 from supabase import create_client, Client
 
+load_dotenv()
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+
+url: str = os.getenv("SUPABASE_URL")
+key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
 
 app = FastAPI()
