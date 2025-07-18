aea955263cdb51055737ec2a00ee8ad7e44bef6b
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-07-19 01:14:33 +0300

    feat: organize frontend directories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dbData, setDbData] = useState({});
 
+  async function fetchData() {
+    try {
+      const response = await axios.get("http://127.0.0.1:8000/patient");
+
+      setDbData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
   return (
     <>
+      <button onClick={fetchData}>Fetch Data</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {dbData.data?.map((data, index) => {
+          return (
+            <div key={index}>
+              <p>{data.id}</p>
+              <p>{data.created_at}</p>
+              <p>{data.age}</p>
+              <p>{data.gender}</p>
+            </div>
+          );
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
