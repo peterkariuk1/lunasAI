af16e477587a87693cb8dc778af475a62f87f86f
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-07-19 01:15:10 +0300

    feat: add login fast api auth route

--- backend/server.py
+++ backend/server.py
@@ -2,15 +2,27 @@ from fastapi import FastAPI
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
+app = FastAPI()
 load_dotenv()
 
+
+origins = [
+    "http://localhost:5173",        
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,
+    allow_methods=["*"],             
+    allow_headers=["*"],              
+)
 
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
-
-app = FastAPI()
 
 @app.get('/')
 def root():
@@ -25,5 +37,24 @@ response = (
 @app.get("/patient")
 def get_patient():
     return(response)
-
 print(response)
+
+class signUpRequest(BaseModel):
+    email: str
+    password: str
+    
+@app.post('/sign_up')
+def signup_user(data: signUpRequest):
+    try:
+        result = supabase.auth.sign_up(
+         {
+        "email": data.email,
+        "password": data.password,
+         }
+        )
+        if result.user:
+            return {"success":True, "message": "Sign up complete. Please check your email to confirm"}
+        else:
+            return{"success": False, "error": result}
+    except Exception as e:
+        return{"success": False, "error": str(e)}
