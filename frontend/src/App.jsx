ba603fae57beb3604247e1ecec6176bcdae76662
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-08-12 16:03:09 +0300

    feat: add conditional renders for the header component depending on auth state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,8 +5,7 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import PrivateRoute from "./components/PrivateRoute";
-import Tests from "./pages/Tests";
-import SpeechRecg from "./components/SpeechRecg";
+import Presenting from "./pages/Presenting";
 
 function App() {
   return (
@@ -14,16 +13,15 @@ function App() {
       <Route path="*" element={<NotFound />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/tests" element={<Tests />} />
-      <Route path="/mic" element={<SpeechRecg/>} />
       <Route
-        path="/lunas"
+        path="/present-patient"
         element={
           <PrivateRoute>
-            <Home />
+            <Presenting />
           </PrivateRoute>
         }
       />
+      <Route path="/" element={<Home />} />
     </Routes>
   );
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import { useAuth } from "../../context/AuthContext";
-import lunasLogo from "../assets/lunaslogo.png";
 import HistoryIcon from "@mui/icons-material/History";
 import LogoutIcon from "@mui/icons-material/Logout";
+import LoginIcon from "@mui/icons-material/Login";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
   const { logout } = useAuth();
@@ -9,6 +10,14 @@ const Header = () => {
   const logoutUser = () => {
     logout();
   };
+
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleNavigateToLogin = () => {
+    navigate("/login");
+  };
+
   return (
     <nav>
       <div className="left">
@@ -16,14 +25,25 @@ const Header = () => {
         <h1>LunasAI</h1>
       </div>
       <div className="right">
-        <button className="history-button">
-          <HistoryIcon />
-          History
-        </button>
-        <button className="logout-button" onClick={logoutUser}>
-          <LogoutIcon />
-          Logout
-        </button>
+        {location.pathname !== "/" ? (
+          <button className="history-button">
+            <HistoryIcon />
+            History
+          </button>
+        ) : (
+          ""
+        )}
+        {location.pathname === "/" ? (
+          <button className="logout-button" onClick={handleNavigateToLogin}>
+            <LoginIcon />
+            Login
+          </button>
+        ) : (
+          <button className="logout-button" onClick={logoutUser}>
+            Logout
+            <LogoutIcon />
+          </button>
+        )}
       </div>
     </nav>
   );
