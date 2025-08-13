523332ee5f740e7fe1130aa5598bffee1cac7af3
peterkariuki-01 <pjeykariuki2003@gmail.com>
2025-08-13 17:58:53 +0300

    feat: add vision ocr google service client

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI,Request,HTTPException
+from fastapi import FastAPI,Request,HTTPException,File, UploadFile
 import os
 from dotenv import load_dotenv
 from supabase import create_client, Client
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from google.cloud import vision
+from fastapi.responses import JSONResponse
+import shutil
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./lunasserviceaccount.json"
+
+
 
 app = FastAPI()
 load_dotenv()
 
 
 origins = [
+    
     "http://localhost:5173",        
 ]
 app.add_middleware(
@@ -23,6 +31,25 @@ app.add_middleware(
 url: str = os.getenv("SUPABASE_URL")
 key: str = os.getenv("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
+
+
+def detect_text_from_image(image_path):
+    """Detects text in the image file."""
+    client = vision.ImageAnnotatorClient()
+
+    with open(image_path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    if texts:
+        return texts[0].description
+    else:
+        return "No text detected."
+    
+
 
 @app.get('/')
 def root():
@@ -87,7 +114,19 @@ def login_user(data: loginRequest):
         return{"success": True, "message": "Login successful"}
     except Exception as e:
         return{"success": False, "error": str(e)}
-    
+
+@app.post("/upload-image/")
+async def upload_image(file: UploadFile = File(...)):
+    temp_path = f"./temp_{file.filename}"
+    with open(temp_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    text_result = detect_text_from_image(temp_path)
+    print(temp_path)
+    os.remove(temp_path)
+    print(text_result)
+
+    return JSONResponse(content={"detected_text": text_result})  
 
         
    
